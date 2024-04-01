31faa91738b00ab7bbe8e59f92f7179480ebd15f
eunsung <eunsung.shin@gmail.com>
2024-04-02 00:25:47 +0900

    초기 디렉토리 구조 설정

---
+++ src/test/java/com/project1/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
