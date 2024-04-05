e1a1cfd17734346eaddf3dcc5b5e8b7d973b2df3
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-05 17:55:14 +0900

    mockAPI 작성

--- src/main/java/com/project1/ticketing/api/controller/MockAPIController.java
+++ src/main/java/com/project1/ticketing/api/controller/MockAPIController.java
@@ -1,12 +1,5 @@
 package com.project1.ticketing.api.controller;
-
-
-import com.project1.ticketing.api.dto.request.PaymentRequestDTO;
-import com.project1.ticketing.api.dto.request.PointRequestDTO;
-import com.project1.ticketing.api.dto.request.ReservationRequestDTO;
 import com.project1.ticketing.api.dto.response.*;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
