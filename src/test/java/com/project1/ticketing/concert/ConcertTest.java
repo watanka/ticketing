21ad89e82cbfd83988843aebc37ed6b391c3d173
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-12 17:46:58 +0900

    concert test코드 작성

--- src/test/java/com/project1/ticketing/concert/ConcertTest.java
+++ src/test/java/com/project1/ticketing/concert/ConcertTest.java
@@ -1,38 +1,82 @@
 package com.project1.ticketing.concert;
 
-import com.project1.ticketing.domain.concert.components.MemoryConcertService;
+import com.project1.ticketing.domain.concert.components.ConcertService;
 import com.project1.ticketing.domain.concert.models.Concert;
+import com.project1.ticketing.domain.concert.models.ConcertTime;
+import com.project1.ticketing.domain.concert.models.Seat;
+import com.project1.ticketing.domain.concert.repository.IConcertRepository;
+import com.project1.ticketing.domain.concert.repository.IConcertTimeRepository;
+import com.project1.ticketing.domain.concert.repository.ISeatRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ConcertTest {
 
-    MemoryConcertService concertService;
-    @Autowired
-    public ConcertTest(MemoryConcertService concertService) {
-        this.concertService = concertService;
+    ConcertService concertService;
+    IConcertRepository concertRepository;
+    IConcertTimeRepository concertTimeRepository;
+    ISeatRepository seatRepository;
+
+    List<Concert> concertList;
+    List<ConcertTime> concertTimeList;
+    List<Seat> seatList;
+
+    @BeforeEach
+    void setup(){
+        concertRepository = Mockito.mock(IConcertRepository.class);
+        concertTimeRepository = Mockito.mock(IConcertTimeRepository.class);
+        seatRepository = Mockito.mock(ISeatRepository.class);
+
+
+        concertService = new ConcertService(
+                concertRepository,
+                concertTimeRepository,
+                seatRepository
+                );
+        concertList = List.of(
+                        new Concert(0L, "나훈아50주년 콘서트"),
+                        new Concert( 1L, "아일릿 데뷔 콘서트")
+        );
+
+        long idCnt = 0;
+        concertTimeList = List.of(
+                new ConcertTime(
+                        idCnt++,
+                        ZonedDateTime.parse("2024/04/13/ 20:30:00 KST", DateTimeFormatter.ofPattern("yyyy/MM/dd/ HH:mm:ss z")),
+                        0L, 50, 9),
+                        new  ConcertTime(
+                                idCnt++,
+                                ZonedDateTime.parse("2024/04/14/ 18:30:00 KST",  DateTimeFormatter.ofPattern("yyyy/MM/dd/ HH:mm:ss z")),
+                                0L, 50, 13)
+                        );
+        long seatNoCnt = 0L;
+        seatList = List.of(
+                new Seat(seatNoCnt++, 50000),
+                new Seat(seatNoCnt++, 80000)
+        );
+
     }
 
     @Test
     @DisplayName("상영중인 콘서트들을 확인한다")
     void listConcerts(){
-        //given
-        concertService.addConcert(new Concert(0L, "나훈아50주년 콘서트"));
-        concertService.addConcert(new Concert( 1L, "아일릿 데뷔 콘서트"));
-
-        List<String> concertList = concertService.getConcertList();
-
-        //when
+        //given & when
+        when(concertRepository.getAll()).thenReturn(concertList);
 
 
         //then
-        assertThat(concertList).isEqualTo(List.of("나훈아50주년 콘서트", "아일릿 데뷔 콘서트"));
+        assertThat(concertService.getConcertList()).isEqualTo(concertList);
+
     }
 
 
@@ -40,16 +84,25 @@ public class ConcertTest {
     @DisplayName("선택한 콘서트의 예약가능한 날짜들을 확인한다.")
     void listConcertTimes(){
         long concertId = 0L;
-
-        List<String> concertTimeList = concertService.getAvailableConcertTimeList(concertId);
-//        assertThat(concertTimeList).isEqualTo
+        //given & when
+        when(concertTimeRepository.getAllByConcertId(concertId)).thenReturn(concertTimeList);
+        //then
+        assertThat(concertService.getAllConcertTimeList(concertId)).isEqualTo(concertTimeList);
     }
 
 
-//    @Test
-//    @DisplayName("선택한 콘서트 날짜에 예약가능한 좌석들을 확인한다.")
-//
-//
+    @Test
+    @DisplayName("선택한 콘서트 날짜에 예약가능한 좌석들을 확인한다.")
+    void listConcertSeats(){
+        long concertId = 0L;
+        long concertTimeId = 0L;
+
+        when(seatRepository.getAllByConcertAndConcertTime(concertId, concertTimeId)).thenReturn(seatList);
+        //then
+        assertThat(concertService.getAvailableSeatList(concertId, concertTimeId)).isEqualTo(seatList);
+
+    }
+
 //    @Test
 //    @DisplayName("좌석의 예약상태를 확인한다.")
 //
