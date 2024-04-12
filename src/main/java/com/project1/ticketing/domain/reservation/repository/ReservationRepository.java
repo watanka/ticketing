2d4b5fd5a85297c42fa08ecd850e2bd7ce65a4e9
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-12 18:00:45 +0900

    우선 돌아가게 만들기.

--- src/main/java/com/project1/ticketing/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/project1/ticketing/domain/reservation/repository/ReservationRepository.java
@@ -1,8 +1,10 @@
 package com.project1.ticketing.domain.reservation.repository;
 
 import com.project1.ticketing.domain.reservation.models.Reservation;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 
 public interface ReservationRepository {
 
