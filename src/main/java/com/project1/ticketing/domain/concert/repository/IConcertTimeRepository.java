7453da7b49b500e9376fc0740bbc2bd42695a4cd
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-15 17:51:57 +0900

    repository 함수명 수정

--- src/main/java/com/project1/ticketing/domain/concert/repository/IConcertTimeRepository.java
+++ src/main/java/com/project1/ticketing/domain/concert/repository/IConcertTimeRepository.java
@@ -4,11 +4,10 @@ import com.project1.ticketing.domain.concert.models.ConcertTime;
 
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface IConcertTimeRepository {
     List<ConcertTime> getAllByConcertId(long concertId);
-    List<ConcertTime> getAvailableTimeByConcertId(long concertId);
-    List<ConcertTime> getByTime(ZonedDateTime dateTime);
-    Boolean isFull(ZonedDateTime dateTime);
+    List<ConcertTime> getAvailableTime(long concertId);
+    ConcertTime getByTime(long concertId, long concertTimeId);
+    Boolean isFull(long concertId, long concertTimeId);
 }
--- src/main/java/com/project1/ticketing/domain/concert/repository/ISeatRepository.java
+++ src/main/java/com/project1/ticketing/domain/concert/repository/ISeatRepository.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ISeatRepository { // seat는 concertTime에 대한 정보를 모른다.
-    boolean isAvailable(long seatId);
-    Optional<Seat> findById(long seatId);
+    public boolean isAvailable(long concertId, long concertTimeId, long seatId);
+    public Optional<Seat> findById(long concertId, long concertTimeId, long seatId);
 
-    List<Seat> getAllByConcertAndConcertTime(long concertId, long concertTimeId);
+    public List<Seat> getAllSeats(long concertHallId);
 }
