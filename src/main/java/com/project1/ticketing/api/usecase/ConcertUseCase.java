09a3bb3bcbf458298b84c0e97498952457137c60
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-15 17:53:07 +0900

    usecase 다시 정의해야함

--- src/main/java/com/project1/ticketing/api/usecase/ConcertUseCase.java
+++ src/main/java/com/project1/ticketing/api/usecase/ConcertUseCase.java
@@ -2,7 +2,7 @@ package com.project1.ticketing.api.usecase;
 
 import com.project1.ticketing.api.dto.response.ConcertResponse;
 
-import com.project1.ticketing.domain.concert.components.MemoryConcertService;
+import com.project1.ticketing.domain.concert.components.ConcertService;
 import com.project1.ticketing.domain.concert.models.Concert;
 import com.project1.ticketing.domain.concert.models.ConcertTime;
 import com.project1.ticketing.domain.concert.models.Seat;
@@ -14,31 +14,11 @@ import java.util.List;
 
 @Component
 public class ConcertUseCase {
-    MemoryConcertService concertService;
+    ConcertService concertService;
 
     @Autowired
-    public ConcertUseCase(MemoryConcertService concertService) {
+    public ConcertUseCase(ConcertService concertService) {
         this.concertService = concertService;
     }
 
-    public ConcertResponse getConcertList(){
-
-        List<Concert> concertList = concertService.getConcertList();
-
-        return new ConcertResponse(concertList);
-
-    }
-
-    public ConcertResponse getAvailableConcertTimeList(long concertId) {
-        List<ConcertTime> availableConcertTimeList = concertService.getAllConcertTimeList(concertId);
-
-        return new ConcertResponse(concertId, availableConcertTimeList);
-    }
-
-    public ConcertResponse getAvailableSeatList(long concertId, long concertTimeId) {
-        List<Seat> availableSeatList = concertService.getAvailableSeatList(concertId, concertTimeId);
-
-        return new ConcertResponse(concertId, concertTimeId, availableSeatList);
-
-    }
 }
