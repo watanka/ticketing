4fc079766d27d6cc7b4696a56c30268211424cb8
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-15 17:52:52 +0900

    concert response 세분화

--- src/main/java/com/project1/ticketing/api/controller/ConcertController.java
+++ src/main/java/com/project1/ticketing/api/controller/ConcertController.java
@@ -1,43 +1,54 @@
 package com.project1.ticketing.api.controller;
 
 import com.project1.ticketing.api.dto.response.ConcertResponse;
-import com.project1.ticketing.api.usecase.ConcertUseCase;
+import com.project1.ticketing.api.dto.response.ConcertTimeResponse;
+import com.project1.ticketing.api.dto.response.SeatResponse;
+import com.project1.ticketing.domain.concert.components.ConcertService;
+import com.project1.ticketing.domain.concert.models.Concert;
+import com.project1.ticketing.domain.concert.models.ConcertTime;
+import com.project1.ticketing.domain.concert.models.Seat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ConcertController {
 
-    ConcertUseCase concertUseCase;
+    ConcertService concertService;
 
     @Autowired
-    public ConcertController(ConcertUseCase concertUseCase) {
-        this.concertUseCase = concertUseCase;
+    public ConcertController(ConcertService concertService) {
+        this.concertService = concertService;
     }
 
     @GetMapping("/concerts")
-    public ConcertResponse getConcertList(){
-        return concertUseCase.getConcertList();
+    public ConcertResponse getAllConcerts(){
+
+//        List<Concert> concertList = concertService.getAllConcerts();
+
+        return null;
     }
 
     @GetMapping("/concerts/{concert_id}/concert-times")
-    public ResponseEntity<ConcertResponse> getAvailableConcertTime(@PathVariable(value="concert_id") long concertId){
+    public ResponseEntity<ConcertTimeResponse> getAvailableConcertTime(@PathVariable(value="concert_id") long concertId){
 
-        ConcertResponse concertResponse = concertUseCase.getAvailableConcertTimeList(concertId);
-
-        return ResponseEntity.ok().body(concertResponse);
-
+//        List<ConcertTime> concertTimes = concertService.getAllTimes(concertId);
+//
+//        return ResponseEntity.ok().body(concertTimeResponse);
+        return null;
     }
 
     @GetMapping("/concerts/{concert_id}/concert-times/{concert_time}/seats")
-    public ResponseEntity<ConcertResponse> getAvailableSeat(
+    public ResponseEntity<SeatResponse> getAvailableSeat(
             @PathVariable(value="concert_id") long concertId,
             @PathVariable(value="concert_time") long concertTimeId){
 
-        ConcertResponse concertResponse = concertUseCase.getAvailableSeatList(concertId, concertTimeId);
-
-        return ResponseEntity.ok().body(concertResponse);
+//        List<Seat> seat = concertService.getAllSeats(concertId, concertTimeId);
+//
+//        return ResponseEntity.ok().body(seatResponse);
+        return null;
 
     }
 }
--- src/main/java/com/project1/ticketing/api/dto/response/ConcertResponse.java
+++ src/main/java/com/project1/ticketing/api/dto/response/ConcertResponse.java
@@ -3,6 +3,7 @@ package com.project1.ticketing.api.dto.response;
 import com.project1.ticketing.domain.concert.models.Concert;
 import com.project1.ticketing.domain.concert.models.ConcertTime;
 import com.project1.ticketing.domain.concert.models.Seat;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,29 +14,16 @@ import java.util.List;
 public class ConcertResponse {
     private List<Concert> concertList;
     private long concertId;
-    private long concertTimeId;
-    private List<ConcertTime> concertTimeList;
-    private long seat;
+    private String concertName;
     private List<Seat> seatList;  // 한 번에 한 좌석만
 
 
     public ConcertResponse(List<Concert> concertList){
-
         this.concertList = concertList;
     }
     public ConcertResponse(long concertId) {
         this.concertId = concertId;
     }
 
-    public ConcertResponse(long concertId, List<ConcertTime> concertTimeList) {
-        this.concertId = concertId;
-        this.concertList = concertList;
-    }
 
-
-    public ConcertResponse(long concertId, long concertTimeId, List<Seat> seatList) {
-        this.concertId = concertId;
-        this.concertTimeId = concertTimeId;
-        this.seatList = seatList;
-    }
 }
