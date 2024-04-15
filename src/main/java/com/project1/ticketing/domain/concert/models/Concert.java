69d771cca32ab7a47195560ad4383ede302823cc
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-04-15 17:51:24 +0900

    테스트시 인스턴스 생성을 위해 builder 추가

--- src/main/java/com/project1/ticketing/domain/concert/models/Concert.java
+++ src/main/java/com/project1/ticketing/domain/concert/models/Concert.java
@@ -1,14 +1,21 @@
 package com.project1.ticketing.domain.concert.models;
 
+import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
+@Builder
 @Getter
 public class Concert{
     private long id;
     private String name;
+    private List<ConcertTime> concertTimeList;
 
-    public Concert(long id, String name) {
+
+    public Concert(long id, String name, List<ConcertTime> concertTimeList) {
         this.id = id;
         this.name = name;
+        this.concertTimeList = concertTimeList;
     }
 }
--- src/main/java/com/project1/ticketing/domain/concert/models/ConcertTime.java
+++ src/main/java/com/project1/ticketing/domain/concert/models/ConcertTime.java
@@ -1,13 +1,27 @@
 package com.project1.ticketing.domain.concert.models;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-public record ConcertTime(
+@Builder
+@Getter
+public class ConcertTime{
 
-        long id,
-        ZonedDateTime time,
-        long concertId,
-        int maxSeatNum,
-        int currAvailableSeatNum
-) {
+    long id;
+    ZonedDateTime time;
+    long concertId;
+    long concertHallId;
+    int maxSeatNum;
+    int currAvailableSeatNum;
+
+    public static ZonedDateTime fromStr(String timeStr){
+        /// str format: "yyyy/MM/dd/ HH:mm:ss z" e.g) "2024/04/13/ 20:30:00 KST"
+        return ZonedDateTime.parse(timeStr, DateTimeFormatter.ofPattern("yyyy/MM/dd/ HH:mm:ss z"));
+    }
+
 }
+
+
