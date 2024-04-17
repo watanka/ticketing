8a16724eeb762b92263da0ab788dca179590a4cb
eunsung <eunsung.shin@gmail.com>
2024-04-17 20:46:20 +0900

    payment 도메인 유닛테스트

--- src/main/java/com/project1/ticketing/domain/payment/components/IPaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.project1.ticketing.domain.payment.components;
-
-import com.project1.ticketing.api.dto.request.PaymentRequest;
-import com.project1.ticketing.api.dto.response.PaymentResponse;
-
-public interface IPaymentService {
-    PaymentResponse pay(PaymentRequest paymentRequest);
-    PaymentResponse checkPayment(long userId, long reservationId);
-}
