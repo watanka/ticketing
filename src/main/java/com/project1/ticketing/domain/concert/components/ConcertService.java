a6f7972dcb345f7db729d4ef3b99d397d0a2a8a5
eunsung <eunsung.shin@gmail.com>
2024-04-19 01:07:05 +0900

    예약 가능 여부 확인하는 filter 컴포넌트 분리

--- src/main/java/com/project1/ticketing/domain/concert/components/ConcertService.java
+++ src/main/java/com/project1/ticketing/domain/concert/components/ConcertService.java
@@ -3,18 +3,13 @@ package com.project1.ticketing.domain.concert.components;
 import com.project1.ticketing.api.dto.response.ConcertResponse;
 import com.project1.ticketing.api.dto.response.ConcertTimeResponse;
 import com.project1.ticketing.api.dto.response.SeatResponse;
-import com.project1.ticketing.api.utils.exceptions.BaseException;
-import com.project1.ticketing.domain.concert.models.Concert;
 import com.project1.ticketing.domain.concert.models.ConcertTime;
 import com.project1.ticketing.domain.concert.models.Seat;
-import com.project1.ticketing.domain.concert.models.SeatStatus;
 import com.project1.ticketing.domain.concert.repository.IConcertHallRepository;
 import com.project1.ticketing.domain.concert.repository.IConcertRepository;
 import com.project1.ticketing.domain.concert.repository.IConcertTimeRepository;
 import com.project1.ticketing.domain.concert.repository.ISeatRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,21 +20,18 @@ public class ConcertService implements IConcertService{
     IConcertTimeRepository concertTimeRepository;
     ISeatRepository seatRepository;
 
+    ConcertValidator concertValidator;
     ConcertFilter concertFilter;
-    IConcertHallRepository concertHallRepository;
 
-    public ConcertService(IConcertRepository concertRepository,
-                          IConcertTimeRepository concertTimeRepository,
-                          ISeatRepository seatRepository,
-                          ConcertFilter concertFilter,
-                          IConcertHallRepository concertHallRepository) {
+    public ConcertService(IConcertRepository concertRepository, IConcertTimeRepository concertTimeRepository, ISeatRepository seatRepository, ConcertValidator concertValidator, ConcertFilter concertFilter) {
         this.concertRepository = concertRepository;
         this.concertTimeRepository = concertTimeRepository;
         this.seatRepository = seatRepository;
+        this.concertValidator = concertValidator;
         this.concertFilter = concertFilter;
-        this.concertHallRepository = concertHallRepository;
     }
 
+    @Override
     public List<ConcertResponse> getAllConcerts() {
 
         return concertRepository.getAll().stream()
@@ -47,27 +39,21 @@ public class ConcertService implements IConcertService{
                 .collect(Collectors.toList());
     }
 
+    @Override
     public List<ConcertTimeResponse> getAvailableTimes(long concertId) {
-        // 전체 콘서트 시간을 불러온다.
+        concertValidator.validateConcert(concertId);
         List<ConcertTime> concertTimeList = concertTimeRepository.getAllByConcertId(concertId);
-        // 예약이 가능한 시간만 필터링한다.
 
-
-        return concertFilter.filterTime(concertTimeList);
+        return concertFilter.filterAvailableTime(concertTimeList);
 
     }
 
+    @Override
     public List<SeatResponse> getAvailableSeats(long concertTimeId) {
-        Optional<ConcertTime> concertTime = concertTimeRepository.getById(concertTimeId);
+        concertValidator.validateConcertTime(concertTimeId);
+        List<Seat> seatList = seatRepository.getByConcertTimeId(concertTimeId);
 
-        if (concertTime.isEmpty()){
-            throw new RuntimeException("해당 콘서트 시간을 찾지 못 했습니다.");
-        }
-
-        long concerTimeId = concertTime.get().getId();
-        List<Seat> seatList = seatRepository.getByConcertTimeId(concerTimeId);
-
-        return concertFilter.filterSeat(seatList);
+        return concertFilter.filterAvailableSeat(seatList);
 
 
 
@@ -76,13 +62,15 @@ public class ConcertService implements IConcertService{
 
     // 콘서트, 시간, 좌석 순 꽉 찼는지 확인
     public boolean isConcertFull(long concertId){
+        concertValidator.validateConcert(concertId);
         return concertRepository.isFull(concertId);
     }
 
 
 
-    public boolean isSeatReserved(long concertId, long concertTime, long seatId){
-        return seatRepository.isAvailable(concertId, concertTime, seatId);
+    public boolean isSeatReserved(long seatId){
+        concertValidator.validateSeat(seatId);
+        return seatRepository.isAvailable(seatId);
     }
 
 }
