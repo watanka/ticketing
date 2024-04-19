18d242e85bc591e7d39f0626b6c4fd45b3fb07ab
eunsung <eunsung.shin@gmail.com>
2024-04-19 22:58:31 +0900

    Concert 도메인 통합테스트 작성

---
+++ src/test/java/com/project1/ticketing/concert/ConcertIntegrationTest.java
@@ -0,0 +1,126 @@
+package com.project1.ticketing.concert;
+
+import com.project1.ticketing.api.dto.response.ConcertResponse;
+import com.project1.ticketing.api.dto.response.ConcertTimeResponse;
+import com.project1.ticketing.api.dto.response.SeatResponse;
+import com.project1.ticketing.domain.concert.components.ConcertService;
+import com.project1.ticketing.domain.concert.models.ConcertTime;
+import com.project1.ticketing.domain.concert.infrastructure.ConcertCoreRepositoryImpl;
+import com.project1.ticketing.domain.concert.models.Concert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class ConcertIntegrationTest {
+
+    @Autowired
+    ConcertCoreRepositoryImpl concertRepositoryImpl;
+
+    @Autowired
+    ConcertService concertService;
+
+    @Autowired
+    TestDataHandler testDataHandler;
+
+    @BeforeEach
+    void setUp(){
+
+        concertRepositoryImpl.deleteAll();
+        // testDataHandler에 시험할 데이터 넣어놓음
+        testDataHandler.settingConcertInfo();
+    }
+
+    @Test
+    void testJpaRepositoryWorking(){
+        Concert concert = Concert.builder().name("나훈아50주년콘서트1").build();
+        Concert savedConcert = concertRepositoryImpl.saveConcert(concert);
+        Concert foundConcert = concertRepositoryImpl.findConcertById(savedConcert.getId()).orElseThrow();
+
+        Assertions.assertThat(foundConcert.getId()).isEqualTo(concert.getId());
+        Assertions.assertThat(foundConcert.getName()).isEqualTo(concert.getName());
+    }
+
+
+    @Test
+    void testSpringJpaRepositoryWorking(){
+        Concert concert = Concert.builder().name("나훈아50주년콘서트2").build();
+        Concert savedConcert = concertRepositoryImpl.saveConcert(concert);
+        Concert foundConcert = concertRepositoryImpl.findConcertById(savedConcert.getId()).orElseThrow();
+
+        assertThat(foundConcert.getId()).isEqualTo(concert.getId());
+        assertThat(foundConcert.getName()).isEqualTo(concert.getName());
+        assertThat(concert).isEqualTo(foundConcert);
+    }
+
+
+    @Test
+    @DisplayName("콘서트 리스트 조회")
+    void testGetConcertList(){
+        Concert concert = Concert.builder()
+                .name("나훈아50주년콘서트")
+                .build();
+        concertRepositoryImpl.saveConcert(concert);
+
+        List<ConcertResponse> concertResponseList = concertService.getAllConcerts();
+        assertThat(concertResponseList.get(0).getName()).isEqualTo("나훈아50주년콘서트");
+
+        }
+
+    @Test
+    @DisplayName("예약 가능한 콘서트 시간 조회")
+    void testGetAvailableConcertTime(){
+        // ConcertTimeList = [ConcertTime1: 전좌석 예약가능, concertTime2: 전좌석 매진]
+
+        List<ConcertTimeResponse> concertTimeResponseList = concertService.getAvailableTimes(1L);
+
+
+        assertThat(concertTimeResponseList.size()).isEqualTo(1);
+        assertThat(concertTimeResponseList.get(0).getTime()).isEqualTo(ConcertTime.fromStr("2024/04/20/ 17:00:00 KST"));
+
+    }
+
+    @Test
+    @DisplayName("콘서트 찾을 수 없음")
+    void testNotExistConcertId(){
+        // ConcertTimeList = [ConcertTime1: 전좌석 예약가능, concertTime2: 전좌석 매진]
+
+        try{
+            List<ConcertTimeResponse> concertTimeResponseList = concertService.getAvailableTimes(3L);
+        }catch (RuntimeException e){
+            assertThat(e.getMessage()).isEqualTo("콘서트 정보를 찾을 수 없습니다.");
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("예약 가능한 좌석 조회")
+    void testGetAvailableSeat(){
+        long concertTimeId = 1L;
+        List<SeatResponse> seatResponseList = concertService.getAvailableSeats(concertTimeId);
+
+        for (SeatResponse seatResponse : seatResponseList) {
+            assertThat(seatResponse.isAvailable()).isEqualTo(true);
+        }
+
+
+    }
+
+
+
+
+
+
+}
--- src/test/java/com/project1/ticketing/concert/TestDataHandler.java
+++ src/test/java/com/project1/ticketing/concert/TestDataHandler.java
@@ -1,8 +1,6 @@
 package com.project1.ticketing.concert;
 
-import com.project1.ticketing.domain.concert.infrastructure.ConcertRepositoryImpl;
-import com.project1.ticketing.domain.concert.infrastructure.ConcertTimeRepositoryImpl;
-import com.project1.ticketing.domain.concert.infrastructure.SeatRepositoryImpl;
+import com.project1.ticketing.domain.concert.infrastructure.ConcertCoreRepositoryImpl;
 import com.project1.ticketing.domain.concert.models.Concert;
 import com.project1.ticketing.domain.concert.models.ConcertTime;
 import com.project1.ticketing.domain.concert.models.Seat;
@@ -16,73 +14,78 @@ import java.util.List;
 public class TestDataHandler {
 
 
-    ConcertRepositoryImpl concertRepositoryImpl;
-    ConcertTimeRepositoryImpl concertTimeRepositoryImpl;
-    SeatRepositoryImpl seatRepositoryImpl;
+    ConcertCoreRepositoryImpl concertRepositoryImpl;
 
 
 
     @Autowired
-    public TestDataHandler(ConcertRepositoryImpl concertRepositoryImpl, ConcertTimeRepositoryImpl concertTimeRepositoryImpl, SeatRepositoryImpl seatRepositoryImpl) {
+    public TestDataHandler(ConcertCoreRepositoryImpl concertRepositoryImpl) {
         this.concertRepositoryImpl = concertRepositoryImpl;
-        this.concertTimeRepositoryImpl = concertTimeRepositoryImpl;
-        this.seatRepositoryImpl = seatRepositoryImpl;
+
     }
 
     void settingConcertInfo(){
 
-        List<Seat> 나훈아_seatList1 = List.of(
+        List<Seat> 전좌석예약가능 = List.of(
                 Seat.builder()
                         .seatNum(0L)
+                        .concertTimeId(1L)
                         .concertHallId(0L)
                         .price(50000)
                         .status(SeatStatus.AVAILABLE)
                         .build(),
                 Seat.builder()
                         .seatNum(1L)
+                        .concertTimeId(1L)
                         .concertHallId(0L)
                         .price(50000)
                         .status(SeatStatus.AVAILABLE)
                         .build(),
                 Seat.builder()
                         .seatNum(2L)
+                        .concertTimeId(1L)
                         .concertHallId(0L)
                         .price(50000)
                         .status(SeatStatus.AVAILABLE)
                         .build()
         );
-        List<Seat> 나훈아_seatList2 = List.of(
+        List<Seat> 전좌석_매진 = List.of(
                 Seat.builder()
                         .seatNum(3L)
+                        .concertTimeId(2L)
                         .concertHallId(0L)
                         .price(50000)
-                        .status(SeatStatus.AVAILABLE)
+                        .status(SeatStatus.RESERVED)
                         .build(),
                 Seat.builder()
                         .seatNum(4L)
+                        .concertTimeId(2L)
                         .concertHallId(0L)
                         .price(50000)
-                        .status(SeatStatus.AVAILABLE)
+                        .status(SeatStatus.RESERVED)
                         .build(),
                 Seat.builder()
                         .seatNum(5L)
+                        .concertTimeId(2L)
                         .concertHallId(0L)
                         .price(50000)
-                        .status(SeatStatus.AVAILABLE)
+                        .status(SeatStatus.RESERVED)
                         .build()
         );
 
         List<ConcertTime> 나훈아_concertTimeList = List.of(
                 ConcertTime.builder()
                         .time(ConcertTime.fromStr("2024/04/20/ 17:00:00 KST"))
-                        .seats(나훈아_seatList1)
+                        .seats(전좌석예약가능)
+                        .concertId(1L)
                         .concertHallId(0L)
                         .maxSeatNum(50)
                         .currAvailableSeatNum(0)
                         .build(),
                 ConcertTime.builder()
                         .time(ConcertTime.fromStr("2024/04/27/ 17:00:00 KST"))
-                        .seats(나훈아_seatList2)
+                        .seats(전좌석_매진)
+                        .concertId(1L) // TODO: 어떻게 넣을 때 연관된 concertID를 인식하고 넣지?
                         .concertHallId(0L)
                         .maxSeatNum(50)
                         .currAvailableSeatNum(0)
@@ -96,16 +99,8 @@ public class TestDataHandler {
                 .build();
 
 
-        concertRepositoryImpl.save(concert);
-        concertTimeRepositoryImpl.save(나훈아_concertTimeList.get(0));
-        concertTimeRepositoryImpl.save(나훈아_concertTimeList.get(1));
+        concertRepositoryImpl.saveConcert(concert);
 
-        for (Seat seat : 나훈아_seatList1) {
-            seatRepositoryImpl.save(seat);
-        }
-        for (Seat seat : 나훈아_seatList2) {
-            seatRepositoryImpl.save(seat);
-        }
 
 
     }
