332c19f9183d37224ba7b024d319f6e7b03f5768
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-05-20 17:54:34 +0900

    mock 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO concert (concert_id, name, created_at, updated_at) VALUES (1, '아일릿 데뷔 콘서트', now(), now());
+INSERT INTO concert (concert_id, name, created_at, updated_at) VALUES (2, '나훈아 50주년 콘서트', now(), now());
+
+INSERT INTO  concert_time (concert_time_id, concert_id, time, created_at, updated_at) VALUES (1, 1, '2024-05-20T18:30:00Z', now(), now());
+INSERT INTO  concert_time (concert_time_id, concert_id, time, created_at, updated_at) VALUES (2, 2, '2024-05-20T18:30:00Z', now(), now());
+
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (1, 1, 500000, 'AVAILABLE', now(), now());
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (2, 1, 300000, 'AVAILABLE', now(), now());
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (3, 1, 200000, 'RESERVED', now(), now());
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (4, 2, 500000, 'RESERVED', now(), now());
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (5, 2, 300000, 'RESERVED', now(), now());
+INSERT INTO seat (seat_num, concert_time_id, price, status, created_at, updated_at) VALUES (6, 2, 200000, 'RESERVED', now(), now());
